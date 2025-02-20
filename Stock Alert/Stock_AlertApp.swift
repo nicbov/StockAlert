2285c8fb2fc137232cde44607f6b7d79c61cfb2b
Nicbov <nico.boving@gmail.com>
2025-02-19 20:32:10 -0700

    Initial Commit

---
+++ Stock Alert/Stock_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_AlertApp.swift
+//  Stock Alert
+//
+//  Created by Nicolas Boving on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Stock_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stock AlertTests/Stock_AlertTests.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_AlertTests.swift
+//  Stock AlertTests
+//
+//  Created by Nicolas Boving on 2/19/25.
+//
+
+import Testing
+@testable import Stock_Alert
+
+struct Stock_AlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
